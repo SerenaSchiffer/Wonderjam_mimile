56649133d31f6859c47e4dae669bff5290ac59ae
Rafaël Côté <raf87crosby@hotmail.com>
2015-02-27 20:17:31 -0500

    Script avec tableau de cases

---
+++ Assets/Scripts/TableauCase.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum EtatCase 
+{
+    Empty,
+    Hero,
+    Enemy,
+    Obstacle,
+    HalfObstacle,
+    Trap 
+};
+
+public class TableauCase : MonoBehaviour {
+
+    public Case[,] tableauCases;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void ChangerEtatCase(EtatCase nouvEtat, int x, int y)
+    {
+        tableauCases[x, y].SetEtat(nouvEtat);
+    }
+}
+
+public class Case : MonoBehaviour
+{
+    private EtatCase etat;
+
+    public void SetEtat(EtatCase etat)
+    {
+        this.etat = etat;
+    }
+
+    public EtatCase GetCase()
+    {
+        return etat;
+    }
+}
