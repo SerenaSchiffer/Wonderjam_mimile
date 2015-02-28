4d7b015a8f8cae771333de6224a06e9b0bcee1a9
Emile Fortin <sylfschiffer@hotmail.com>
2015-02-27 19:19:28 -0500

    no message

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+
+
+	///<summary>
+	/// Tente de vérifier si la case visée est navigable
+	/// </summary>
+
+
+
+}
