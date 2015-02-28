180fb5e25042e787b7bb53ff5463f1385534dc2f
Emile Fortin <sylfschiffer@hotmail.com>
2015-02-28 13:40:35 -0500

    Ajout de script pour avoir des items

--- Assets/Scripts/Weapons/GetItem.cs
+++ Assets/Scripts/Weapons/GetItem.cs
@@ -17,36 +17,40 @@ public class GetItem : MonoBehaviour {
 	public WeaponType weaponType;
 	public int posX;
 	public int posY;
+	private HeroScript hero;
+	private bool itemTaken;
 
 
 	public void GiveWeaponToPlayer(WeaponType weaponType)
 	{
-		switch (weaponType) 
-		{
-		case(WeaponType.Rock) :
-			{
-				//Permettre l'utilisation dans l'interface
-				break;
-			}
-		case(WeaponType.Bone) :
-			{
-				//Permettre l'utilisation dans l'interface
-				break;
-			}
-		case(WeaponType.Jack) :
-			{
-				//Permettre l'utilisation dans l'interface
-				break;
-			}
-		case(WeaponType.Marble) :
-			{
-				//Permettre l'utilisation dans l'interface
-				break;
-			}
-		case(WeaponType.Cat) :
-			{
-				//Permettre l'utilisation dans l'interface
-				break;
+		if (!itemTaken) {
+			switch (weaponType) {
+			case(WeaponType.Rock):
+				{
+					//Permettre l'utilisation dans l'interface
+					Debug.Log ("Tu as pris une roche !");
+					break;
+				}
+			case(WeaponType.Bone):
+				{
+					//Permettre l'utilisation dans l'interface
+					break;
+				}
+			case(WeaponType.Jack):
+				{
+					//Permettre l'utilisation dans l'interface
+					break;
+				}
+			case(WeaponType.Marble):
+				{
+					//Permettre l'utilisation dans l'interface
+					break;
+				}
+			case(WeaponType.Cat):
+				{
+					//Permettre l'utilisation dans l'interface
+					break;
+				}
 			}
 		}
 
@@ -57,11 +61,38 @@ public class GetItem : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		hero = GameObject.FindObjectOfType<HeroScript>();
 	}
-	
+
+	void OnMouseUp(){
+		//Vérification de la distance
+		int posHeroX = hero.mouvements.posX;
+		int posHeroY = hero.mouvements.posY;
+		if (posX == posHeroX && posY == posHeroY + 1) 
+		{
+			GiveWeaponToPlayer (weaponType);
+			itemTaken=true;
+		}
+		if (posX == posHeroX && posY == posHeroY - 1)
+		{
+			GiveWeaponToPlayer (weaponType);
+			itemTaken=true;
+		}
+		if (posY == posHeroY && posX == posHeroX + 1)
+		{
+			GiveWeaponToPlayer (weaponType);
+			itemTaken=true;
+		}
+		if (posY == posHeroY && posX == posHeroX - 1)
+		{
+			GiveWeaponToPlayer (weaponType);
+			itemTaken=true;
+		};
+	}
+
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
