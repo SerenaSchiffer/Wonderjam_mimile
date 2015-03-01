34226e83cbf95a921a2432d66f0125b4b0ab2754
Steven Gagne <totris92@gmail.com>
2015-03-01 14:26:16 -0500

    fin

--- Assets/Scripts/ending_script.cs
+++ Assets/Scripts/ending_script.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class ending_script : MonoBehaviour {
 
+	public bool isset;
+	public bool isset_2;
+	public Texture2D atexture,another;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -12,4 +16,29 @@ public class ending_script : MonoBehaviour {
 	void Update () {
 	
 	}
+
+	void OnGUI()
+	{
+		Invoke ("load", 2);
+
+		if (isset) 
+			GUI.DrawTexture (new Rect ((Screen.width / 2) - (300), (Screen.height / 2) - (500), 931, 1024), atexture, ScaleMode.StretchToFill, true, 10.0F);
+
+		Invoke ("load_2", 5);
+
+		if(isset_2)
+			GUI.DrawTexture (new Rect ((Screen.width/2)-(450), (Screen.height/2)-(500), 931, 1024), another, ScaleMode.StretchToFill, true, 10.0F);
+
+	}
+
+	public void load()
+	{
+		isset = true;
+	}
+
+	public void load_2()
+	{
+		isset_2 = true;
+	}
+
 }
