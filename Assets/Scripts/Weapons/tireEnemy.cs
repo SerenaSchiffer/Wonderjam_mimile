316443421b3e464f8ade1f432d28493a2c7e077a
David Turcotte <ici_c_david@hotmail.com>
2015-02-28 20:10:54 -0500

    Revert "no message"
    This reverts commit 03b0df08ed376f7701629cafb2cde1352219daf0.

--- Assets/Scripts/Weapons/tireEnemy.cs
+++ Assets/Scripts/Weapons/tireEnemy.cs
@@ -43,10 +43,10 @@ public class tireEnemy : MonoBehaviour {
 		case "Degat":{
 			if(hero.mouvements.posX>cible.mouvements.posX) {
 				int dep =hero.mouvements.posX;
-				compare =dep -cible.mouvements.posX;
+				compare =dep -cible.mouvements.posX+1;
 			}else{
 				int dep =cible.mouvements.posX;
-				compare =dep -hero.mouvements.posX;
+				compare =dep -hero.mouvements.posX-1;
 			}
 			if (compare <= arme.distance)
 			{
